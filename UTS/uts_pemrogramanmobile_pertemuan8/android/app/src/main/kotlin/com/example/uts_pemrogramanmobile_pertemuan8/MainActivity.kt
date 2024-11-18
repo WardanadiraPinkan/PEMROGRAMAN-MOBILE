e02c4b297430b3520b1e1c18a229a8414520112f
Wardanadira Pinkan <wardanadira04@gmail.com>
2024-11-18 22:32:50 +0700

    PROJECT 12 DAN SISA YANG BELOM TERUPLOAD - Wardanadira Pinkan Dwiyuwanda

---
+++ UTS/uts_pemrogramanmobile_pertemuan8/android/app/src/main/kotlin/com/example/uts_pemrogramanmobile_pertemuan8/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uts_pemrogramanmobile_pertemuan8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ project12/flutter_api/lib/constants.dart
@@ -0,0 +1,3 @@
+const String weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey";
+const String apiKey = "2d6c883d7710b79c4a300d43b8376de2"; // API Key Anda
+const String cityName = "Malang";
